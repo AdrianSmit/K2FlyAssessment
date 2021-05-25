608dc9ceff8a42290fbd9ea6a83b5bfe127ad392
AdrianSmit <novaasussteam@gmail.com>
2021-05-25 11:42:21 +0200

    Added models and enums

---
+++ Common/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2FlyAssessment.Common
+{
+    public class Enums
+    {
+        public enum FlowerState
+        {
+            OPENING,
+            OPENED,
+            CLOSING,
+            CLOSED
+        }
+    }
+}
---
+++ Models/Flower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static K2FlyAssessment.Common.Enums;
+
+namespace K2FlyAssessment.Models
+{
+    public partial class Flower
+    {
+        public FlowerState flowerState { get; set; }
+        public Color color { get; set; }
+    }
+}
